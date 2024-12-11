66cb4d6848a7d1f293497c7fe10a487dd2290938
aktdaaaa <aktdaaaa@gmail.com>
2024-12-11 23:32:26 +0900

    feat(nvim): ドットファイルを表示する

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,21 +1,26 @@
 return {
-	lazy = true,
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-	},
-	keys = {
-		{ "<leader>e", "<cmd>Neotree toggle reveal<cr>", desc = "NeoTree" },
-	},
-	opts = {
-		filesystem = {
-			bind_to_cwd = false,
-			follow_current_file = { enabled = true },
-			use_libuv_file_watcher = true,
-		},
-	},
+  lazy = true,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  keys = {
+    { "<leader>e", "<cmd>Neotree toggle reveal<cr>", desc = "NeoTree" },
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+        hide_dotfiles = false,
+        hide_gitignored = true,
+      },
+      bind_to_cwd = false,
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
+    },
+  },
 }
