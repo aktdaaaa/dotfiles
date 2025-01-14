e198008bd207df4ec7ffaa702e09da3882bc02f0
aktdaaaa <aktdaaaa@gmail.com>
2025-01-14 23:09:23 +0900

    feat: add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,57 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+local act = wezterm.action
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "TokyoNight Storm"
+
+-- 背景ぼかし
+config.window_background_opacity = 0.3
+config.macos_window_background_blur = 20
+config.tab_bar_at_bottom = true
+
+config.font = wezterm.font("FiraCode Nerd Font Mono", { weight = "Bold" })
+config.font_size = 15.0
+
+config.keys = {
+  {
+    key = "f",
+    mods = "CTRL|CMD",
+    action = act.ToggleFullScreen,
+  },
+  {
+    key = "d",
+    mods = "CMD",
+    action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+  },
+  -- Ctrl+Shift+dで新しいペインを作成(画面を分割)
+  {
+    key = "d",
+    mods = "SHIFT|CMD",
+    action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+  },
+  {
+    key = "[",
+    mods = "CMD",
+    action = act.ActivatePaneDirection("Prev"),
+  },
+  {
+    key = "]",
+    mods = "CMD",
+    action = act.ActivatePaneDirection("Next"),
+  },
+  {
+    key = "w",
+    mods = "CMD",
+    action = act.CloseCurrentPane({ confirm = true }),
+  },
+}
+
+-- and finally, return the configuration to wezterm
+return config
