c80cae86cb66e7ab13d036b8af39f05a27a0d9ef
aktdaaaa <44158403+aktdaaaa@users.noreply.github.com>
2025-02-21 03:25:10 +0900

    feat: コマンドモードでの補完で大文字/小文字の区別をなくす

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -3,11 +3,11 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out =
-    vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+      vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -110,4 +110,18 @@ require("lazy").setup({
   -- install = { colorscheme = { "dracula" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
+})
+
+-- コマンドモードに入ったときだけignorecaseを有効化
+vim.api.nvim_create_autocmd("CmdlineEnter", {
+  callback = function()
+    vim.opt.ignorecase = true
+  end,
+})
+
+-- コマンドモードを抜けたらignorecaseを無効化
+vim.api.nvim_create_autocmd("CmdlineLeave", {
+  callback = function()
+    vim.opt.ignorecase = false
+  end,
 })
