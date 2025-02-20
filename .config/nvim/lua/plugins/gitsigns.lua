13d6ec9146e470d2ba4bf84ad3168eb41bf34424
aktdaaaa <44158403+aktdaaaa@users.noreply.github.com>
2025-02-21 02:25:50 +0900

    dev: format

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,9 +1,9 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	event = { "BufNewFile", "BufRead" },
-	opts = {},
-	keys = {
-		{ "hn", "<cmd>Gitsigns next_hunk<cr>", desc = "next hunk" },
-		{ "hp", "<cmd>Gitsigns prev_hunk<cr>", desc = "prev hunk" },
-	},
+  "lewis6991/gitsigns.nvim",
+  event = { "BufNewFile", "BufRead" },
+  opts = {},
+  keys = {
+    { "nh", "<cmd>Gitsigns next_hunk<cr>", desc = "next hunk" },
+    { "ph", "<cmd>Gitsigns prev_hunk<cr>", desc = "prev hunk" },
+  },
 }
